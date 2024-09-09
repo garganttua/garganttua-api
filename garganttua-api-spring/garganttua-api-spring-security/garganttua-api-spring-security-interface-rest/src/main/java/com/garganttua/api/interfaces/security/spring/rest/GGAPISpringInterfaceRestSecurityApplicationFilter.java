f32d58b6d9c68880121fa649c09aeb16e2cf92f5
Jérémy Colombet <jeremy.colombet@garganttua.com>
2024-09-09 16:16:21 +0200

    Do not execute filter for non API endpoints

--- garganttua-api-spring/garganttua-api-spring-security/garganttua-api-spring-security-interface-rest/src/main/java/com/garganttua/api/interfaces/security/spring/rest/GGAPISpringInterfaceRestSecurityApplicationFilter.java
+++ garganttua-api-spring/garganttua-api-spring-security/garganttua-api-spring-security-interface-rest/src/main/java/com/garganttua/api/interfaces/security/spring/rest/GGAPISpringInterfaceRestSecurityApplicationFilter.java
@@ -31,42 +31,44 @@ public class GGAPISpringInterfaceRestSecurityApplicationFilter extends OncePerRe
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
 			throws ServletException, IOException {
 
-		IGGAPICaller caller = (IGGAPICaller) request.getAttribute(GGAPICallerFilter.CALLER_ATTRIBUTE_NAME);
-		
-		if( caller.getDomain().getSecurity().isAuthenticatorEntity() 
-				&& (
-						request.getMethod().equals("POST") 
-						|| request.getMethod().equals("PATCH") 
-						|| request.getMethod().equals("PUT")
-					) 
-		) {
-			if( log.isDebugEnabled() ) {
-				log.debug("Applying security on authenticator entity "+caller.getDomain().getEntity().getValue0().getSimpleName());
-			}
-			
-			
-			ServletInputStream stream = request.getInputStream();
-			ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+		if (((HttpServletRequest) request).getServletPath().startsWith("/api")) {
 
-			int nRead;
-			byte[] data = new byte[16384];
-
-			while ((nRead = stream.read(data, 0, data.length)) != -1) {
-			  buffer.write(data, 0, nRead);
-			}
-
-			byte[] entityAsByteArray = buffer.toByteArray();
+			IGGAPICaller caller = (IGGAPICaller) request.getAttribute(GGAPICallerFilter.CALLER_ATTRIBUTE_NAME);
 			
-			ObjectMapper mapper = new ObjectMapper();
-			Object entity = mapper.readValue(entityAsByteArray, caller.getDomain().getEntity().getValue0());
-			
-			try {
-				this.security.applySecurityOnAuthenticatorEntity(entity);
-			} catch (GGAPIException e) {
-				throw new IOException(e);
+			if( caller.getDomain().getSecurity().isAuthenticatorEntity() 
+					&& (
+							request.getMethod().equals("POST") 
+							|| request.getMethod().equals("PATCH") 
+							|| request.getMethod().equals("PUT")
+						) 
+			) {
+				if( log.isDebugEnabled() ) {
+					log.debug("Applying security on authenticator entity "+caller.getDomain().getEntity().getValue0().getSimpleName());
+				}
+				
+				
+				ServletInputStream stream = request.getInputStream();
+				ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+	
+				int nRead;
+				byte[] data = new byte[16384];
+	
+				while ((nRead = stream.read(data, 0, data.length)) != -1) {
+				  buffer.write(data, 0, nRead);
+				}
+	
+				byte[] entityAsByteArray = buffer.toByteArray();
+				
+				ObjectMapper mapper = new ObjectMapper();
+				Object entity = mapper.readValue(entityAsByteArray, caller.getDomain().getEntity().getValue0());
+				
+				try {
+					this.security.applySecurityOnAuthenticatorEntity(entity);
+				} catch (GGAPIException e) {
+					throw new IOException(e);
+				}
 			}
 		}
-
 		filterChain.doFilter(request, response);
 	}
 }
