69617797a6c9e076a61a26e530fd52f7cc7208d6
Jérémy Colombet <jeremy.colombet@garganttua.com>
2024-10-11 13:15:27 +0200

    bug fix on bean injection

--- garganttua-api-spring/garganttua-api-spring-security/garganttua-api-spring-security-interfaces/garganttua-api-spring-security-interface-rest/src/main/java/com/garganttua/api/interfaces/security/spring/rest/GGAPISpringWithAuthorizationManagerGenericFilter.java
+++ garganttua-api-spring/garganttua-api-spring-security/garganttua-api-spring-security-interfaces/garganttua-api-spring-security-interface-rest/src/main/java/com/garganttua/api/interfaces/security/spring/rest/GGAPISpringWithAuthorizationManagerGenericFilter.java
@@ -4,10 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import com.garganttua.api.interfaces.spring.rest.GGAPICallerFilter;
 import com.garganttua.api.interfaces.spring.rest.GGAPISpringHttpApiFilter;
-import com.garganttua.api.security.core.engine.GGAPISecurityEngine;
 import com.garganttua.api.spec.GGAPIException;
 import com.garganttua.api.spec.caller.IGGAPICaller;
 import com.garganttua.api.spec.security.IGGAPIAuthorizationManagerIfPresentMethod;
+import com.garganttua.api.spec.security.IGGAPISecurityEngine;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -15,7 +15,7 @@ import jakarta.servlet.http.HttpServletResponse;
 public abstract class GGAPISpringWithAuthorizationManagerGenericFilter extends GGAPISpringHttpApiFilter implements IGGAPIAuthorizationManagerIfPresentMethod {
 	
 	@Autowired 
-	protected GGAPISecurityEngine security;
+	protected IGGAPISecurityEngine security;
 	
 	@Override
 	protected void doFilter(HttpServletRequest request, HttpServletResponse response) throws GGAPIException {
